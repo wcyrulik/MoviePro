167e311773522baee38e01bd670821e204969a56
wcyrulik <36646884+wcyrulik@users.noreply.github.com>
2022-04-08 20:52:34 -0400

    Initial commit

---
+++ Models/Database/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MoviePro.Models.Database
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<MovieCollection> MovieCollections
+            { get; set; } = new HashSet<MovieCollection>();
+
+    }
+}
